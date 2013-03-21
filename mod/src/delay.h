792458e820773fe92c11f4c47bcbed105b9d7151
Alex Wong <wangchunfeng192@163.com>
2013-03-21 11:11:09 +0800

    Create delay.h

---
+++ mod/src/delay.h
@@ -0,0 +1,45 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+#ifndef DELAY_H
+#define DELAY_H
+
+
+
+#include "common.h"
+
+
+
+void SetTimer100us(UINT16 timeOut);
+void Delay1s(UINT8 delay);
+void Delay1ms(UINT32 delay);
+void Delay256P3us(UINT8 delay);
+void Delay256P2us(UINT8 delay);
+void Delay256us(UINT8 delay);
+void Delay1us(UINT32 delay);
+//void PollTimerInit(void);
+//void RestartPollTimer(void);
+//void PollTimer_Uninit(void);
+
+
+
+
+
+#endif
+
+
+
+
+
+
+
